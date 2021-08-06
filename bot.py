65efc634720d8d4975a16c3326870cc9ef5f11ce
EwenBALOUIN <46872095+EwenBALOUIN@users.noreply.github.com>
2021-08-07 01:32:05 +0200

    :sparkles: add security for the main user

--- bot.py
+++ bot.py
@@ -7,17 +7,34 @@ from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
+MY_USER_ID = os.getenv('MY_USER_ID')
 
 bot = commands.Bot(command_prefix='$')
 
 @bot.command(name='miaou', help='Miaou from an angry Gizmo')
 async def miaou(ctx):
+    author = ctx.message.author
+    if author.id != int(MY_USER_ID):
+        await not_good_person(author)
+        return
     miaou = "Miaou 😺"
     await ctx.send(miaou)
 
 @bot.command(name='wouaf', help='Wouaf from an angry Gizmo')
 async def wouaf(ctx):
+    author = ctx.message.author
+    if author.id != int(MY_USER_ID):
+        await not_good_person(author)
+        return
     wouaf = "Bon toutou 🐶"
     await ctx.send(wouaf)
+
+
+async def not_good_person(author):
+        await author.create_dm()
+        await author.dm_channel.send(
+            "T'as pas le droit FDP"
+        )
+
 
 bot.run(TOKEN)
